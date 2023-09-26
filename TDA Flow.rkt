8c9567ac3f46262a8562fe9fae879bee28872306
Ka Zeng <ka.zeng@usach.cl>
2023-09-26 20:08:02 -0300

    flow-add-option

--- TDA Flow.rkt
+++ TDA Flow.rkt
@@ -4,16 +4,33 @@
 
 (require racket/date)
 
+; flow
 (define (flow id name-msg . options)
   (if (unique-options? options)
       (list id name-msg options)
-      (error "Las ID de opciones deben ser unicas")))
+      (error "Los opciones de ID deben ser unicas")))
 
 (define (unique-options? options)
-  (let ((ids (map (lambda (option) (car option)) options)))
-    (equal? (length ids) (length (remove-duplicates ids)))))
+  (equal? (length (option-ids options)) 
+          (length (remove-duplicates (option-ids options)))))
+
+(define (option-ids options)
+  (map car options))
 
 (define (remove-duplicates lst)
   (cond ((null? lst) '())
         ((member (car lst) (cdr lst)) (remove-duplicates (cdr lst)))
         (else (cons (car lst) (remove-duplicates (cdr lst))))))
+
+; flow-add-option
+(define (flow-add-option flow option)
+  (if (option-exists? (caddr flow) (car option))
+      (begin
+        (display "Error: La opcion de ID ya existe en el flujo.\n")
+        flow)  
+      (list (car flow) (cadr flow) (append (caddr flow) (list option))) 
+  )
+)
+
+(define (option-exists? flow-options option-id)
+  (member option-id (map car flow-options)))
