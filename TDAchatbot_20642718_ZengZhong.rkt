c7cd7a9dafb8311926dac2e30ae43e11572c5984
Ka Zeng <ka.zeng@usach.cl>
2023-10-03 19:27:05 -0300

    script de pruebas agregado

--- TDAchatbot_20642718_ZengZhong.rkt
+++ TDAchatbot_20642718_ZengZhong.rkt
@@ -2,39 +2,30 @@
 
 (provide (all-defined-out))
 
-; chatbot - constructor
-#|(define (chatbot chatbotID name welcomeMessage startFlowId . flows)
-  (list chatbotID name welcomeMessage startFlowId (unique-flows flows)))
-
-(define (unique-flows flows)
-  (foldl add-if-unique '() (flatten flows)))
-
-(define (add-if-unique acc flow)
-  (if (not (flow-exists? flow acc))
-      (cons flow acc)
-      acc))
-
-(define (flow-exists? flow flows)
-  (cond ((null? flows) #f)
-        ((equal? (car flow) (car (car flows))) #t)
-        (else (flow-exists? flow (cdr flows)))))
-|#
+; chatbot - constuctor
 (define (chatbot chatbotID name welcomeMessage startFlowId . flows)
   (list chatbotID name welcomeMessage startFlowId (unique-flows flows)))
 
 (define (unique-flows flows)
-  (foldl add-if-unique '() flows))
+  (remove-duplicates2 flows flow-id-equal?))
 
-(define (add-if-unique acc flow)
-  (if (not (flow-exists? flow acc))
-      (cons flow acc)
-      acc))
+(define (flow-id-equal? flow1 flow2)
+  (eq? (car flow1) (car flow2)))
 
-(define (flow-exists? flow flows)
-  (cond ((null? flows) #f)
-        ((null? (car flows)) #f) ; Aquí nos aseguramos de que el car de flows no sea nulo
-        ((equal? (car flow) (car (car flows))) #t)
-        (else (flow-exists? flow (cdr flows)))))
+(define (remove-duplicates2 lst equal?)
+  (foldl (lambda (item acc)
+           (if (any (lambda (x) (equal? x item)) acc)
+               acc
+               (cons item acc)))
+         '()
+         lst))
+
+(define (any pred lst)
+  (cond ((null? lst) #f)
+        ((pred (car lst)) #t)
+        (else (any pred (cdr lst)))))
+
+
 
 
 ; chatbot - modificador
@@ -45,3 +36,13 @@
         (caddr chatbot)
         (cadddr chatbot)
         (append (car (cddddr chatbot)) (list (add-if-unique flow (car (cddddr chatbot)))))))
+
+(define (add-if-unique acc flow)
+  (if (not (flow-exists? flow acc))
+      (cons flow acc)
+      acc))
+
+(define (flow-exists? flow flows)
+  (cond ((null? flows) #f)
+        ((equal? (car flow) (car (car flows))) #t)
+        (else (flow-exists? flow (cdr flows)))))
--- TDAsystem_20642718_ZengZhong.rkt
+++ TDAsystem_20642718_ZengZhong.rkt
@@ -17,18 +17,26 @@
         fechaFinal))
 
 ; add-chatbot
+;; Verifica si un chatbot ya existe en el sistema
 (define (chatbot-exists? system chatbot)
   (ormap (lambda (existing-chatbot)
            (= (car existing-chatbot) (car chatbot)))
          (caddr system)))
 
+;; Añade un chatbot al sistema si no existe
 (define (system-add-chatbot system chatbot)
-  (if (chatbot-exists? system chatbot)
-      system
-      (cons (car system)
-            (cons (cadr system)
-                  (cons (append (caddr system) (list chatbot))
-                        (cdr (cddr system)))))))
+  (cond
+    ;; Si el chatbot ya existe, muestra un mensaje de error y devuelve el sistema sin cambios
+    ((chatbot-exists? system chatbot)
+     (display "El chatbot ya existe.\n")
+     system)
+
+    ;; En caso contrario, añade el chatbot al sistema
+    (else
+     (list (car system)                   ; Nombre del sistema
+           (cadr system)                  ; InitialChatbotCodeLink
+           (append (caddr system) (list chatbot)) ; Lista de chatbots con el nuevo chatbot añadido
+           (cadddr system)))))            ; Lista de usuarios
 
 ; add-user
 (define (user-exists? system user)
@@ -37,24 +45,34 @@
 
 (define (system-add-user system user)
   (if (user-exists? system user)
-      system  
-      (list (car system)  
-            (cadr system) 
-            (caddr system)  
-            (append (cadddr system) (list user)) 
-            )))
+      (begin
+        (display "El usuario ya existe.\n")
+        system)  ; Si el usuario ya existe, simplemente devuelve el sistema sin cambios
+      (list (car system)        ; Nombre del sistema
+            (cadr system)       ; InitialChatbotCodeLink
+            (caddr system)      ; Lista de chatbots
+            (append (cadddr system) (list user)) ; Añadir el nuevo usuario a la lista de usuarios
+            (list-ref system 4) ; chatHistory
+            (if (>= (length system) 6) (list-ref system 5) #f)))) ; Mantener el usuario logueado (si hay alguno) o #f si no hay sesión iniciada
+
 
 ; login
 (define (system-login system user)
-  (if (or (not (user-exists? system user))  ; Si el usuario no existe en el sistema
-          (list-ref system 5))  ; o si alguien ya ha iniciado sesión (la sexta parte del sistema es el nombre del usuario)
-      system  ; Devolver el sistema tal como está
-      (list (car system)     ; Nombre del sistema
-            (cadr system)    ; InitialChatbotCodeLink
-            (caddr system)   ; Lista de chatbots
-            (cadddr system)  ; Lista de usuarios
-            (list-ref system 4)  ; chatHistory
-            user)))  ; Establecer el usuario que ha iniciado sesión
+  (cond
+    ;; Si el usuario no existe en el sistema o alguien ya ha iniciado sesión
+    ((or (not (user-exists? system user))
+         (and (>= (length system) 6) (list-ref system 5)))
+     (display "No se pudo iniciar sesion.\n")
+     system) ; Devolver el sistema tal como está
+
+    ;; En caso contrario, establecer el usuario que ha iniciado sesión
+    (else
+     (list (car system)      ; Nombre del sistema
+           (cadr system)     ; InitialChatbotCodeLink
+           (caddr system)    ; Lista de chatbots
+           (cadddr system)   ; Lista de usuarios
+           (list-ref system 4) ; chatHistory
+           user))))  ; Establecer el usuario que ha iniciado sesión
 
 ; logout
 (define (system-logout system)
